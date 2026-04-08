42e6c9e0b9c4c46e926aedd677c922aa04962b2a
Chris <chrissdev.io@gmail.com>
2026-04-08 15:19:25 -0700

    I added a new function.

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
   const mainDiv = document.querySelector('#container');
    
     function createGrid(size){
-        for(let i = 0; i < size; i++){
+        let squareSize = 960 / size ; 
+        for(let i  = 0; i < size * size; i++){
 
     const newDiv = document.createElement('div');
       mainDiv.appendChild(newDiv);
       newDiv.addEventListener('mouseover',()=>{
         newDiv.style.background = 'red'; })
+        newDiv.style.width = squareSize + 'px';
+        newDiv.style.height = squareSize + 'px';
         }
     }
     createGrid(16);
